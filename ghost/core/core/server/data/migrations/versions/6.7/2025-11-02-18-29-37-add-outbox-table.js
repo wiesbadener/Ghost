fbfa52658f0f03cc6cf97969dd136dd966cd6689
Troy Ciesco <tmciesco@gmail.com>
2025-10-29 14:55:12 -0400

    updates to outbox spike

--- ghost/core/core/server/data/migrations/versions/6.7/2025-11-02-18-29-37-add-outbox-table.js
+++ ghost/core/core/server/data/migrations/versions/6.7/2025-11-02-18-29-37-add-outbox-table.js
@@ -5,6 +5,7 @@ module.exports = addTable('outbox', {
     event_type: {type: 'string', maxlength: 50, nullable: false},
     status: {type: 'string', maxlength: 20, nullable: false, defaultTo: 'pending'},
     payload: {type: 'text', maxlength: 65535, nullable: false},
+    status: {type: 'string', maxlength: 20, nullable: false, defaultTo: 'pending'},
     created_at: {type: 'dateTime', nullable: false},
     retry_count: {type: 'integer', nullable: false, defaultTo: 0},
     last_retry_at: {type: 'dateTime', nullable: true},
